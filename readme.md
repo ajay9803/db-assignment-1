7a72f7ca202e2663c646865530c29c3a693a702c
ajay9803 <ajayprajapati9803@gmail.com>
2024-07-05 08:49:50 +0545

    Update readme.md

---
+++ readme.md
@@ -0,0 +1,12 @@
+# Product and Order Database Setup
+
+This guide provides instructions for creating and populating a simple product and order database. It also includes steps for updating a product's category and executing queries to analyze order data.
+
+## Database Schema tables
+
+The `Products` table contains information about the products available in the inventory.
+The `Orders` table contains information about the orders made by the customer for product.
+
+## Results
+
+The `results` can be found in the db-assignment-1.pdf with both: The query and result.
--- script.sql
+++ script.sql
@@ -1,3 +1,4 @@
+-- create products-table
 CREATE TABLE Products (
     product_id SERIAL PRIMARY KEY,  
     product_name VARCHAR(50),       
@@ -5,7 +6,7 @@ CREATE TABLE Products (
     price INT                      
 );
 
-
+-- create orders-table
 create table Orders (
     order_id SERIAL primary key,        
     customer_name VARCHAR(100),          
@@ -14,7 +15,7 @@ create table Orders (
     order_date TIMESTAMP                 
 );
 
-
+-- insert values to products-table
 INSERT INTO Products (product_name, Category, price) VALUES
 ('Laptop', 'Electronics', 1200),
 ('Headphones', 'Electronics', 150),
@@ -22,7 +23,7 @@ INSERT INTO Products (product_name, Category, price) VALUES
 ('Book', 'Books', 20),
 ('Office Chair', 'Furniture', 200);
 
-
+-- insert values to orders-table
 INSERT INTO Orders (customer_name, product_id, quantity, order_date) VALUES
 ('Ram', 1, 2, '2024-07-01 10:30:00'),
 ('Hari', 2, 1, '2024-07-02 14:45:00'),
@@ -30,12 +31,14 @@ INSERT INTO Orders (customer_name, product_id, quantity, order_date) VALUES
 ('Mandra', 4, 5, '2024-07-03 11:15:00'),
 ('Meow', 5, 1, '2024-07-04 16:20:00');
 
-
+-- update a particular product
 UPDATE Products SET category = 'hardware' WHERE product_id = 1;
 
+--  total quantity ordered for each product category in the orders table
 select o.product_id, (select product_name from products where product_id = o.product_id), sum(quantity)
 from orders o group by product_id order by o.product_id;
 
+-- categories where the total number of products ordered is greater than 5
 select * from products p 
 where product_id = (select product_id from orders group by product_id having sum(quantity) > 5)
 
